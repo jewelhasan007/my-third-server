b499340b226bd5b14ae28768fc2abed04b8ec0f2
Jewel Hasan <jewel.hasan@cg-bd.com>
2024-11-04 16:44:56 +0600

    DELETE API

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = 5000
+const port = 3000
 
 // middleware
 app.use(cors())
@@ -11,6 +11,7 @@ app.use(express.json())
 // zigzagminded
 // oIi6kgZE8WiUSobB
 // WcvDb3yM2d7pWe8G
+// PsDRERS3jR1VrfRM
 
 // const users = [
 //     {name: "korim", emial: "korim@gmail.com"}
@@ -33,8 +34,6 @@ res.send('the server is running')
 
 // MondoDb connection
 
-
-
 const uri = "mongodb+srv://zigzagminded:WcvDb3yM2d7pWe8G@cluster0.0frmz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -51,26 +50,41 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const database = client.db("usersDB2");
-    const usersCollections = database.collection("users");
+    const usersCollections = client.db("usersDB2").collection("users");
 
+// CREATE User from client 
     app.post('/users', async(req, res)=>{
       const user = req.body;
       console.log('new user inside async function run', user)
       const result = await usersCollections.insertOne(user);
       res.send(result)
     })
- 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  // READ Users from mongoDB to client
+app.get('/users', async(req, res)=>{
+  const user = usersCollections.find()
+  const result = await user.toArray()
+  res.send(result) 
+})
+
+// DELETE users from mongoDb
+app.delete('/users/:id', async(req, res)=>{
+  const newId = req.params.id;
+    console.log('delete id in server', newId)
+    const query = {_id: new ObjectId(newId)}
+    const result = await usersCollections.deleteOne(query)
+    res.send(result)
+  })
+
+// Send a ping to confirm a successful connection
+await client.db("admin").command({ ping: 1 });
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, ()=>{
     console.log(`the port is running${port}`)
